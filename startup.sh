ad9a4ce3d3426ebe643ff8aa27fbe6a443796dba
Josh Peck <josh@truecodecapital.com>
2026-02-18 00:24:53 -0800

    adjusting startup script via testing various host OSes

--- startup.sh
+++ startup.sh
@@ -6,6 +6,14 @@ echo "🚀 Starting trueclaw bootstrap..."
 # Update system
 apt-get update
 
+# set up GPU connections
+apt upgrade -y
+apt install -y build-essential dkms linux-headers-$(uname -r)
+apt install firmware-misc-nonfree nvidia-driver -y
+apt update
+sed -i '/^Components:/ s/$/ contrib non-free-firmware non-free/' /etc/apt/sources.list.d/debian.sources
+
+# install build dependencies
 apt install build-essential g++ clang libssl-dev -y
 
 echo "alias ll='ls -lFa'" >> ~/.bashrc
